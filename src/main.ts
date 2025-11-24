7a27ca718f6a6867ba0118385ebc84a70df28dca
Josh Fisher <jfisherd@gmail.com>
2025-11-24 16:22:33 -0500

    docs: main.ts add DOM logic to create new cards

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
 import { contactApi } from "./services/apiService.js"
 
+const header = document.getElementById('header')
+const nav = document.getElementById('nav')
+const main = document.getElementById('main')
+
 const myCountries = await contactApi()
 
+for (let i = 0; i < myCountries.length; i++) {
+    let myNewCard = document.createElement('div')
+    myNewCard.setAttribute('class', 'card')
+    myNewCard.setAttribute('id', `card-${i}`)
+    let myNewCardBody = document.createElement('div')
+    main?.append(myNewCard)
+}
